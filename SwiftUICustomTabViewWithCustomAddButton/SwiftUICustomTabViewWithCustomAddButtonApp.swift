b4d5be554895bb9d546e2f14379cf16c053b168e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-08 13:29:30 +0200

    Initial Commit

---
+++ SwiftUICustomTabViewWithCustomAddButton/SwiftUICustomTabViewWithCustomAddButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabViewWithCustomAddButtonApp.swift
+//  SwiftUICustomTabViewWithCustomAddButton
+//
+//  Created by ramil on 08.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabViewWithCustomAddButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
